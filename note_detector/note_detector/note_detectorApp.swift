e0b03fd935aa91d9284e5be9753c8164af6a0524
peppperrroni <peppperrroni@gmail.com>
2024-11-08 13:14:56 +0000

    Added project

---
+++ note_detector/note_detector/note_detectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  note_detectorApp.swift
+//  note_detector
+//
+//  Created by Yuriy Egorov on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct note_detectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ note_detector/note_detectorTests/note_detectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  note_detectorTests.swift
+//  note_detectorTests
+//
+//  Created by Yuriy Egorov on 11/8/24.
+//
+
+import Testing
+@testable import note_detector
+
+struct note_detectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
